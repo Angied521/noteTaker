43b61bfb1d2ea05fc54dff4fcb5a0c81a1b8d49b
Angie DePasquale <angied521@yahoo.com>
2020-02-05 11:52:38 -0500

    folders fixed and route fixed.

--- server.js
+++ server.js
@@ -1,25 +1,67 @@
 const express = require("express");
+const notes = require('./db.json')
 
+const PORT = 5500;
 
-const PORT = 8000; 
 const app = express();
+const path = require("path");
+
+app.use(express.static('public'))
 
 
-//routes
-app.get("/", (req, res) => {
-    res.send("Hello world");
+//router to return the notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+//api routes
+//router to save a unique id
+app.get("/test/:id", (req, res) => {
+    res.render("test", { output: req.params.id })
+});
+
+
+//API routes
+//get /api/notes to read the db.js file and return saved notes
+
+//route to the db.json file
+//app.get('/api/notes/:id', (req, res) => {
+//    res.sendfile(path.join(_dirname, '/db.js')); 
+
+//});
+//all notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 
 });
 
-app.get("/", (req, res) => {
-    res.json();
+//post api notes receive new note,save on the request body and add to db.json then return to new note client
+app.post('/api/notes', (req, res) => {
+    res.send(notes);
+});
+
+
+//Delete api/notes by :id
+//read the note fron the db.json file 
+//remove by given id
+// rewrite the note to the db.json file
+app.delete("/api/notes/:id", (req, res) => {
+    res.send(note);
 });
 
 
 
 
+
+//router to retun the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+//listener
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
 });
 
-
+module.exports = app
